6eef130bd007697119d1f110daff58ce191fa082
SamHerts <sdherts@gmail.com>
2023-03-23 12:37:44 -0500

    add tank entity

---
+++ game/include/TankEntity.h
@@ -0,0 +1,21 @@
+//
+// Created by SamHerts on 3/21/2023.
+//
+
+#ifndef BULLETTANK_TANKENTITY_H
+#define BULLETTANK_TANKENTITY_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+class TankEntity
+{
+public:
+    int x_position;
+    int y_position;
+    SDL_Texture* texture;
+
+};
+
+
+#endif //BULLETTANK_TANKENTITY_H
---
+++ game/src/TankEntity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SamHerts on 3/21/2023.
+//
+
+#include "../include/TankEntity.h"
